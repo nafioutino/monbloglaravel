a9f31c69400d5e0d57a43e1138ef94e3bb15f356
Abdoul-Nafiou <nafiou531@gmail.com>
2024-07-22 10:29:37 +0100

    @yield @extnend @section

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -1,1 +1,7 @@
+
+@extends('layouts.master')
+
+
+@section('content')
 <h2>Contacez-nous</h2>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
-    <h1>Mon blog Laravel</h1>
+<h1>Mon blog Laravel</h1>
     <p>
         <a href="/contact-us">Contacez-nous</a>
     </p>
+    
+
+<!--Contenue de tout les pages ici -->
+@yield('content')
+<!--Contenue de tout les pages ici -->
+
 </body>
+</html>
