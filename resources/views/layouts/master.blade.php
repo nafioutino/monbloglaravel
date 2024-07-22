811086375bc64a6dd35ebf09d14e3ed8563054f7
Abdoul-Nafiou <nafiou531@gmail.com>
2024-07-22 10:12:29 +0100

    layout contact créé

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,6 @@
+<body>
+    <h1>Mon blog Laravel</h1>
+    <p>
+        <a href="/contact-us">Contacez-nous</a>
+    </p>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.master');
+});
+Route::get('/contact-us', function () {
+    return view('layouts.contact');
 });
